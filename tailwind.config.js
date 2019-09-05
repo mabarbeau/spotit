d81ccdb9cf33f12afc920b4a256bb9b2a94c1be2
Marc Barbeau <marcbarbeau@MARCB343.local>
2019-09-05 11:24:47 -0600

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  important: true,
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        red: '#911',
+      },
+    },
+  },
+  variants: {
+    opacity: ['responsive', 'hover'],
+  },
+};
