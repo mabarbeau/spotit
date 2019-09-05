fb788e09047e2754318afe1a140e38d1c188923e
Marc Barbeau <marcbarbeau@MARCB343.local>
2019-09-05 16:16:21 -0600

    Create app footer

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,22 @@
   >
     <app-header />
     <main class="flex-1 relative">
-    <router-view />
+      <router-view />
     </main>
+    <app-footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import AppHeader from '@/layout/Header.vue';
+import AppFooter from '@/layout/Footer.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
     AppHeader,
+    AppFooter,
   },
 });
 </script>
---
+++ src/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="flex">
+    Footer
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppHeader',
+});
+</script>
