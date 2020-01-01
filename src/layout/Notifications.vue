53102aa23376af69fef2f202320c5a248a792bcd
Marc Barbeau <mabarbeau1@gmail.com>
2020-01-01 14:59:24 -0800

    Create notifications

---
+++ src/layout/Notifications.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    id="nav"
+    class="bg-gray-800 px-5 py-3 flex justify-between"
+  >
+    Notifications
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+})
+</script>
