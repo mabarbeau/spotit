ae64b7a21fbd3b269f291bd0d99fba61562ea6cc
Marc Barbeau <mabarbeau1@gmail.com>
2020-02-11 19:08:52 -0800

    Add error handling

--- src/state/modules/auth.ts
+++ src/state/modules/auth.ts
@@ -1,33 +1,40 @@
 import Api from '@/api'
+import { ActionContext } from 'vuex/types/index.d'
 
-interface UserInterface {
+interface User {
   name: string
 }
 
-interface AuthStateInterface {
-  me: UserInterface | Object
+interface AuthModule {
+  me: User | undefined
 }
 
-export const state: AuthStateInterface = {
-  me: {},
+export const state: AuthModule = {
+  me: undefined,
 }
 
 export const getters = {}
 
 export const mutations = {
-  SET_ME(state: AuthStateInterface, user: UserInterface) {
+  SET_ME(state: AuthModule, user: User) {
     state.me = user
   },
 }
 
 export const actions = {
-  async login({ commit }: any, payload: any) {
-    commit(
-      'SET_ME',
-      await Api.post({
-        name: 'auth.login',
-        payload,
+  async login(
+    { commit, dispatch }: any,
+    payload: ActionContext<AuthModule, any>
+  ) {
+    await Api.post({
+      name: 'auth.login',
+      payload,
+    })
+      .then((user) => {
+        commit('SET_ME', user)
       })
-    )
+      .catch((error: Error) => {
+        dispatch('errors/set', error, { root: true })
+      })
   },
 }
--- src/state/modules/spots.ts
+++ src/state/modules/spots.ts
@@ -1,17 +1,18 @@
 import Api from '@/api'
+import { ActionContext } from 'vuex/types/index.d'
 
-interface SpotsInterface {}
+interface Spots {}
 
-interface SpotsCollectionInterface {
-  data: SpotsInterface[]
+interface SpotsCollection {
+  data: Spots[]
 }
 
-interface SpotsStateInterface {
-  spot: SpotsInterface | Object
-  spots: SpotsCollectionInterface | Object
+interface SpotsModule {
+  spot: Spots | Object
+  spots: SpotsCollection | Object
 }
 
-export const state: SpotsStateInterface = {
+export const state: SpotsModule = {
   spot: {},
   spots: {},
 }
@@ -19,31 +20,40 @@ export const state: SpotsStateInterface = {
 export const getters = {}
 
 export const mutations = {
-  SET_SPOTS(state: SpotsStateInterface, spots: SpotsCollectionInterface) {
+  SET_SPOTS(state: SpotsModule, spots: SpotsCollection) {
     state.spots = spots
   },
-  SET_SPOT(state: SpotsStateInterface, spot: SpotsInterface) {
+  SET_SPOT(state: SpotsModule, spot: Spots) {
     state.spot = spot
   },
 }
 
 export const actions = {
-  async loadSpots({ commit }: any) {
-    commit(
-      'SET_SPOTS',
-      await Api.get({
-        name: 'spots.all',
-        payload: window.location.search,
+  async loadSpots({ commit, dispatch }: ActionContext<SpotsModule, any>) {
+    await Api.get({
+      name: 'spots.all',
+      payload: window.location.search,
+    })
+      .then((spots) => {
+        commit('SET_SPOTS', spots)
       })
-    )
+      .catch((error: Error) => {
+        dispatch('errors/set', error, { root: true })
+      })
   },
-  async loadSpot({ commit }: any, slug: string) {
-    commit(
-      'SET_SPOT',
-      await Api.get({
-        name: 'spots.find',
-        params: { slug },
+  async loadSpot(
+    { commit, dispatch }: ActionContext<SpotsModule, any>,
+    slug: string
+  ) {
+    await Api.get({
+      name: 'spots.find',
+      params: { slug },
+    })
+      .then((spot) => {
+        commit('SET_SPOT', spot)
       })
-    )
+      .catch((error: Error) => {
+        dispatch('errors/set', error, { root: true })
+      })
   },
 }
