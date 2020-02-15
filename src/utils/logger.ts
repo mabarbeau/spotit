9e7a4f44caac0dda8b3e4b47378e87de3e2fe6cf
Marc Barbeau <mabarbeau1@gmail.com>
2020-02-14 17:05:41 -0800

    Don't freak out

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,31 @@
 import axios from 'axios'
 
-if (!process.env.VUE_APP_LOGGER) throw new Error('API is undefined')
-const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_LOGGER,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+if (!process.env.VUE_APP_LOGGER) {
+  console.warn('Warning: external logging not setup')
+}
+const axiosInstance = process.env.VUE_APP_LOGGER
+  ? axios.create({
+      baseURL: process.env.VUE_APP_LOGGER,
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  : undefined
 
 export default class Logger {
   public static async info(data: any) {
-    axiosInstance.post('/log/info', { info: JSON.stringify(data) })
+    if (axiosInstance)
+      axiosInstance.post('/log/info', { info: JSON.stringify(data) })
   }
 
   public static async warning(data: any) {
-    axiosInstance.post('/log/warning', { warning: JSON.stringify(data) })
+    if (axiosInstance)
+      axiosInstance.post('/log/warning', { warning: JSON.stringify(data) })
   }
 
   public static async error(data: any) {
-    axiosInstance.post('/log/error', { error: JSON.stringify(data) })
+    if (axiosInstance)
+      axiosInstance.post('/log/error', { error: JSON.stringify(data) })
   }
 }
