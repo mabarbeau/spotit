345ee24900668962326d63a1ff62e20bac82182c
Marc Barbeau <mabarbeau1@gmail.com>
2020-02-14 19:59:59 -0800

    Move folder

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,11 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
-import AppConfirm from '@/layout/Confirm.vue'
+import AppConfirm from '@/views/layout/Confirm.vue'
 import AppError from '@/views/errors/Index.vue'
-import AppFooter from '@/layout/Footer.vue'
-import AppHeader from '@/layout/Header.vue'
-import AppNotification from '@/layout/Notification.vue'
+import AppFooter from '@/views/layout/Footer.vue'
+import AppHeader from '@/views/layout/Header.vue'
+import AppNotification from '@/views/layout/Notification.vue'
 
 export default Vue.extend({
   name: 'App',
