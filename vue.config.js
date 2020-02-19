c5b593f9de5f778f08352a6740ebf27dd200f26d
Marc Barbeau <mabarbeau1@gmail.com>
2020-02-18 18:22:50 -0800

    Use https

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const fs = require('fs')
+
 module.exports = {
   devServer: {
     host: process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : 'localhost',
+    https: process.env.VUE_APP_CERT
+      ? {
+          key: fs.readFileSync(`./cert/${process.env.VUE_APP_CERT}-key.pem`),
+          cert: fs.readFileSync(`./cert/${process.env.VUE_APP_CERT}.pem`),
+        }
+      : false,
+    public: `${
+      process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : 'localhost'
+    }:8080/`,
   },
   css: {
     sourceMap: true,
