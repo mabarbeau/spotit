34479f7a1adff93824c5f680a026464d48587bce
Marc Barbeau <mabarbeau1@gmail.com>
2020-02-23 16:14:41 -0800

    Create confirm test

--- src/state/modules/confirm.spec.ts
+++ src/state/modules/confirm.spec.ts
@@ -1,13 +1,15 @@
-import { mutations } from './confirm'
+import { getters, mutations, actions } from './confirm'
 
 const { SET_MESSAGE, DELETE_MESSAGE } = mutations
+const { confirm, confirmed, canceled } = actions
+
+const message = 'test message'
+const state = {
+  message: undefined,
+  resolve: undefined,
+}
 
 describe('mutations', () => {
-  const message = 'test message'
-  const state = {
-    message: undefined,
-    resolve: undefined,
-  }
   it('sets a message', () => {
     SET_MESSAGE(state, message)
     expect(state.message).toEqual(message)
@@ -15,5 +17,37 @@ describe('mutations', () => {
   it('deletes a message', () => {
     DELETE_MESSAGE(state)
     expect(state.message).toEqual(undefined)
+  })
+})
+
+describe('actions', () => {
+  const context = {
+    dispatch: jest.fn(),
+    commit: jest.fn(),
+    state,
+    getters,
+    rootState: {},
+    rootGetters: {},
+  }
+
+  it('returns a promise', () => {
+    const response = confirm(context, message)
+    expect(response instanceof Promise).toBe(true)
+  })
+  it('resolves when "confirmed" is called', () => {
+    const localState = {
+      message,
+      resolve: jest.fn(),
+    }
+    confirmed({ ...context, ...{ state: localState } })
+    expect(localState.resolve).toBeCalled()
+  })
+  it('resolves when "canceled" is called', () => {
+    const localState = {
+      message,
+      resolve: jest.fn(),
+    }
+    canceled({ ...context, ...{ state: localState } })
+    expect(localState.resolve).toBeCalled()
   })
 })
