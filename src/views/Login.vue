7037ac8bee1c0c05e7d8381416f70a6102e19ca2
Marc Barbeau <mabarbeau1@gmail.com>
2020-03-29 11:28:44 -0700

    Extract google login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="absolute h-full w-full flex justify-center">
     <div class=" w-full max-w-xs self-center pb-10">
-      <div id="google-login-button" />
+      <app-login-google />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import render from '@/utils/gapis'
+import AppLoginGoogle from '@/components/AppLoginGoogle.vue'
 
 export default Vue.extend({
-  name: 'Login',
-  mounted() {
-    render('google-login-button', {
-      onsuccess: this.onSuccess,
-      onfailure: this.onFailure,
-    })
-  },
-  methods: {
-    async onSuccess(user: any) {
-      await this.$store.dispatch('auth/login', {
-        service: 'google',
-        token: user.getAuthResponse().id_token,
-      })
-    },
-    onFailure(error: any) {
-      console.log(error)
-    },
+  components: {
+    AppLoginGoogle,
   },
 })
 </script>
